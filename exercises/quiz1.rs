07da579e616ea560e778df9ae4eddb07a594e102
williamli <williamli@xiaoe-tech.com>
2023-09-12 19:05:49 +0800

    exercises:230912

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: u8) -> u8 {
+    if num <= 40 {
+        num * 2
+    } else {
+        num * 1
+    }
+}
 
 // Don't modify this function!
 #[test]
